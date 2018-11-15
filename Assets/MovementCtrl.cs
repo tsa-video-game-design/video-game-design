928e09dfc05061cbb79797b03b0d1e9836cfeb02
tsa-video-game-design <44676423+tsa-video-game-design@users.noreply.github.com>
2018-11-15 08:31:58 -0800

    Roll_A_Ball_test
    Please don't merge this, this is a test. (Doesn't even work)

---
+++ Assets/MovementCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
